ee378e49d9f507101dec45b72829fc4cbc27d75a
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 12:49:02 -0300

    fix: Enabled CORS for fetching data

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: 'http://localhost:3000'});
   await app.listen(process.env.NODE_PORT || 5000);
 }
 bootstrap();
