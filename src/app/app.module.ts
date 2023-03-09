804933bd6bfdae97200c3dd566619e1daee78184
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 10:33:19 -0300

    feat: Set the environment variables globally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GithubController } from 'src/models/github/github.controller';
 import { AppService } from './app.service';
+import { GithubService } from 'src/models/github/github.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: `.env.${process.env.NODE_ENV}`,
+    isGlobal: true, 
   })],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, GithubController],
+  providers: [AppService, GithubService],
 })
 
 export class AppModule {}
