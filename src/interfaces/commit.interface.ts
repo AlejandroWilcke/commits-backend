9c7724aa404696d45c8dfaee6422b1e70956ddea
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 10:37:18 -0300

    feat: Added github interface

---
+++ src/interfaces/commit.interface.ts
@@ -0,0 +1,13 @@
+interface ICommitAuthor {
+  handle: string;
+  alias: string;
+  email: string;
+  profile_url: string;
+  avatar_url: string;
+}
+
+export default interface ICommit {
+  author: ICommitAuthor;
+  date: string;
+  message: string;
+}
