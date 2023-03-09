cf80a6694c30c5124889b98882c7cabfd2703c83
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-09 10:37:51 -0300

    feat: Added octokit service for fetching commits from repo

---
+++ src/config/octokit.ts
@@ -0,0 +1,34 @@
+import { Octokit } from "octokit";
+import ICommit from "src/interfaces/commit.interface";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+class OctokitService {
+  static async getCommits(repo: string, branch: string = 'master'): Promise<ICommit[]>{
+    try{
+      let { data } = await octokit.request(`GET /repos/${process.env.GITHUB_OWNER}/${repo}/commits`, {
+        sha: branch,
+        headers: { 'X-GitHub-Api-Version': '2022-11-28' }
+      });
+      return data.map( (el: any) => {
+        return{
+          author: {
+            handle: el.author?.login,
+            alias: el.commit?.author?.name,
+            email: el.commit?.author?.email,
+            profile_url: el.author?.html_url,
+            avatar_url: el.author?.avatar_url
+          },
+          date: el.commit?.author?.date,
+          message: el.commit?.message,
+        }
+      });
+    }catch(error){
+      return error.message;
+    }
+  }
+}
+
+export default OctokitService;
